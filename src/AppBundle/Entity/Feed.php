e2c44e9948cd2109046bed7b2aaaba4954fb8790
Luis San Martín <lsansaa@gmail.com>
2017-07-13 15:22:49 -0400

    Creación de bases de datos, entidades (Archivo, Proyecto, Persona, Director, Trabajador, RepresentanteLegal, ProyectoTrabajador, Feed). Faltan atributos de Archivo, Persona y Feed

---
+++ src/AppBundle/Entity/Feed.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Feed
+ *
+ * @ORM\Table(name="feed")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\FeedRepository")
+ */
+class Feed
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
---
+++ src/AppBundle/Entity/Persona.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Persona
+ *
+ * @ORM\Table(name="persona")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PersonaRepository")
+ */
+class Persona
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="rut", type="string")
+     * @ORM\Id
+     */
+    private $rut;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=255, nullable=true)
+     */
+    private $nombre;
+
+
+    /**
+     * Get Rut
+     *
+     * @return string
+     */
+    public function getRut()
+    {
+        return $this->rut;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Persona
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
+
