89ff77d3c0e122d7313edaaa3f81d3887c2a9c71
RodrigoPizarro <rpz001@alumnos.ucn.cl>
2017-07-27 12:54:51 -0400

    Update: Ahora /proyectos desplegará la lista de proyectos según la persona. Si es un administrador, puede ver todos los proyectos. Si es un usuario, solo puede ver los proyectos en que el esté participando, ya sea como director, representante o trabajador común.
    Pendiente: Hace falta hacer pruebas con las credenciales validadas en la base de datos, no en memoria.

--- src/AppBundle/Controller/ProjectController.php
+++ src/AppBundle/Controller/ProjectController.php
@@ -17,6 +17,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityRepository;
 
 class ProjectController extends Controller {
 
@@ -82,35 +83,58 @@ class ProjectController extends Controller {
      */
     public function proyectos(Request $request){
 
+        $sesion = $this->getUser();
         $proyecto = new Proyecto();
 
         $form = $this->createFormBuilder($proyecto)->getForm();
         $form->handleRequest($request);
 
-        if ($form->isSubmitted()) {
+        //Acceso por medio de una credencial guardada en memoria. (No obtenida de la BD).
+        if(strcmp($sesion,'luis') == 0){
 
-            $nombre_proyecto = $request->get('nombre_proyecto');
-            return new Response("<html><body>$nombre_proyecto</body></html>");
+            $proyectos = $this->getDoctrine()->getRepository(Proyecto::class)->findAll();
+            return $this->render('default/proyectos.html.twig',array('proyectos' => $proyectos, 'form' => $form->createView()));
 
-            $proyecto = $this->getDoctrine()
-                ->getRepository(Proyecto::class)
-                ->findOneBy(array(
-                    'nombre' => $nombre_proyecto
-                ));
-            $archivos = $this->getDoctrine()
-                ->getRepository(Archivo::class)
-                ->findBy(array(
-                    'id_proyecto' => $proyecto
-                ));
-            return $this->render('default/proyecto.html.twig', array(
-                'proyecto'=>$proyecto,
-                'archivos'=>$archivos
-            ));
+        }else{ //De lo contrario, se busca a la persona en la BD usando el rut o email de la sesión.
+
+            $personas = $this->getDoctrine()->getRepository(Persona::class)->findAll();
+
+            foreach($personas as $persona){
+
+                if(strcmp($persona->getRut(),$sesion) == 0 or strcmp($persona->getEmail(),$sesion) == 0){
+
+                    //Si la persona es administrador, tiene acceso a todos los proyectos.
+                    if(strcmp($persona->getRol(),"ROLE_ADMIN") == 0){
+
+                        $proyectos = $this->getDoctrine()->getRepository(Proyecto::class)->findAll();
+                        return $this->render('default/proyectos.html.twig',array('proyectos' => $proyectos, 'form' => $form->createView()));
+
+                        //De lo contrario, solo tiene acceso a los proyectos que participe, ya sea como director, representante o trabajador comun.
+                    }elseif(strcmp($persona->getRol(),"ROLE_USER") == 0){
+
+                        //Obteniendo todos los proyectos en que el usuario es director.
+                        $proyectosDirector = $this->getDoctrine()->getRepository(Proyecto::class)->findByRutDirector($persona);
+
+                        //Obteniendo todos los proyectos en que el usuario es representante.
+                        $proyectosRepresentante = $this->getDoctrine()->getRepository(Proyecto::class)->findByRutRepresentante($persona);
+
+                        //Obteniendo todos los proyectos en que el usuario es trabajador comun.
+                        $proyectosTrabajador = $this->getDoctrine()->getRepository(ProyectoTrabajador::class)->findByRutTrabajador($persona);
+
+                        //Se guardan todos los proyectos en que participa en una sola lista.
+                        $proyectos = array_merge($proyectosDirector,$proyectosRepresentante,$proyectosTrabajador);
+
+                        return $this->render('default/proyectos.html.twig',array('proyectos' => $proyectos, 'form' => $form->createView()));
+
+                    }
+
+                }
+
+            }
 
         }
 
-        $proyectos = $this->getDoctrine()->getRepository(Proyecto::class)->findAll();
-        return $this->render('default/proyectos.html.twig',array('proyectos' => $proyectos, 'form' => $form->createView()));
+        return new Response("<html><body><h1>NO EXISTE EL USUARIO</h1></body></html>");
 
     }
 
