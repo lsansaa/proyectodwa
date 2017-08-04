5bc9e85749dcfec50391b108cf97b7c5fc32ac05
RodrigoPizarro <rpz001@alumnos.ucn.cl>
2017-08-04 19:41:56 -0400

    Validación del contraseña corregida.

--- src/AppBundle/Resources/js/validaciones.js
+++ src/AppBundle/Resources/js/validaciones.js
@@ -114,8 +114,8 @@ $( document ).ready(function() {
     });
 });
 function validarPassword(password){
-    return true;
-    //return /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8}$/i.test(password)
+    //return true;
+    return /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8}$/i.test(password)
 }
 function validarEmail(password){
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(password)
