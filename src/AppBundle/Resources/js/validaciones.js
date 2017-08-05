909f00866adb5c2aee8e7656b0daed10492b2aaf
Luis San Martín <lsansaa@gmail.com>
2017-08-05 00:18:39 -0400

    Corrección en la validación del RUT.

--- src/AppBundle/Resources/js/validaciones.js
+++ src/AppBundle/Resources/js/validaciones.js
@@ -22,21 +22,20 @@ $( document ).ready(function() {
         }
     });
     $(".soloRut").bind('blur',function(){
-        var rut = this.value
+        var rut = this.value;
         var noSpaces = rut.replace(/\s/g, "");
-        if(validarRut(rut)){
-            var parent = $(this).parent();
-            parent.attr('class','input-group has-success');
-            this.setCustomValidity("");
-        }
-        else if(noSpaces.length==0){
+        if(noSpaces.length==0){
             var parent = $(this).parent();
             parent.attr('class','input-group');
+            this.setCustomValidity("");
+        }else if(validarRut(rut)){
+            var parent = $(this).parent();
+            parent.attr('class','input-group has-success');
             this.setCustomValidity("");
         }else{
             var parent = $(this).parent();
             parent.attr('class','input-group has-error');
-            this.setCustomValidity("RUT no valido");
+            this.setCustomValidity("RUT no válido");
         }
         if (!this.checkValidity()){
             //console.log('Error handling');
@@ -111,11 +110,61 @@ $( document ).ready(function() {
     });
 });
 function validarPassword(password){
-    return /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8}$/i.test(password)
+    return /^(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])(?=.*[a-z]).{8}$/i.test(password);
 }
-function validarEmail(password){
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(password)
+function validarEmail(email){
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
-function validarRut(b){
-    if(b.match(/^([0-9])+\-([kK0-9])+$/)){b=b.split("-");var a=b[0].split(""),c=2,d=0;for(i=a.length-1;0<=i;i--)c=7<c?2:c,d+=parseInt(a[i])*parseInt(c++);a=11-d%11;return(11==a?0:10==a?"k":a)==b[1].toLowerCase()}return!1
+function validarRut(rut){
+        if(rut){
+            //eliminar el guion
+            rut = rut.replace('-','');
+            //eliminar puntos
+            rut = rut.replace(/\./g,'');
+            //cuerpo del rut
+            var cuerpo = rut.slice(0,-1);
+            //digito verificador
+            var digitoVerificador = rut.slice(-1).toUpperCase();
+            //si el cuerpo no cumple con el largo mínimo (7), retorna false
+            if(cuerpo.length<7){
+                return false;
+            }
+            //verificación del digito
+
+            var suma = 0;
+            var multiplo = 2;
+
+            //Se recorre cada digito del cuerpo, de atrás hacia adelantema
+            for(var i=0;i<cuerpo.length;i++){
+                //y se multiplica cada uno por el multiplo, para agregarse después a la suma
+                var productoActual = multiplo * cuerpo.charAt(cuerpo.length-i-1);
+                suma = suma + productoActual;
+                //se aumenta el valor del multiplo
+                multiplo++;
+                if(multiplo==8){
+                    multiplo = 2;
+                }
+            }
+            //se divide el valor obtenido de la suma por 11, para obtener el resto
+            var resto = suma % 11;
+            //por ultimo se calcula la diferencia entre 11 y el resto
+            var diferencia = 11- resto;
+            var digitoVerificadorReal = "";
+            // si la diferencia es 11, el digito verificador debería ser 0
+            if(diferencia == 11){
+                digitoVerificadorReal = "0";
+            }
+            // si la diferencia es 10, el digito verificador debería ser k
+            else if(diferencia == 10){
+                digitoVerificadorReal = "K";
+            }
+            // si la diferencia es 10, el digito verificador debería ser k
+            else{
+                digitoVerificadorReal = diferencia;
+            }
+
+            return (digitoVerificadorReal==digitoVerificador);
+        }else{
+            return false;
+        }
 }
